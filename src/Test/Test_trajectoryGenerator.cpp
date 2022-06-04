37bb62040a23ed461d5252bb2984205cffaa38d6
Jaehoon-An <dkswogns46@gmail.com>
2022-06-04 22:56:56 +0900

    made quintic trajectory generator

--- src/Test/Test_trajectoryGenerator.cpp
+++ src/Test/Test_trajectoryGenerator.cpp
@@ -4,19 +4,53 @@
 #include <Eigen/Eigen>
 #include <iostream>
 #include "include/TrajectoryGenerator/CubicTrajectoryGenerator.h"
+#include "include/TrajectoryGenerator/QuinticTrajectoryGenerator.h"
 
-int main() {
+void CubicTrajectoryTest(double currentPosition, double goalPosition, double currentTime, double timeDuration)
+{
     CubicTrajectoryGenerator trajGen;
-    trajGen.updateTrajectory(0.0, 0.53, 0.0, 1.0);
-    double currentTime = 0.0;
+    trajGen.updateTrajectory(currentPosition, goalPosition, currentTime, timeDuration);
+    double realTime;
     double accumulatedPosition = 0.0;
-    for(int i = 0; i< 101; i++)
+    double accumulatedVelocity = 0.0;
+    double dT = 0.001;
+    for(int i = 0; i< timeDuration / dT +1; i++)
     {
-        currentTime = i * 0.01;
-        std::cout<<trajGen.getPositionTrajectory(currentTime)<<std::endl;
-        std::cout<<trajGen.getVelocityTrajectory(currentTime)<<std::endl;
-        accumulatedPosition += trajGen.getVelocityTrajectory(currentTime) * 0.01;
-        std::cout<<trajGen.getAccelerationTrajectory(currentTime)<<std::endl;
+        realTime = i * dT;
+        std::cout<<"iteration : "<<i<<std::endl;
+        std::cout<<"desired position : "<<trajGen.getPositionTrajectory(realTime)<<std::endl;
+        std::cout<<"desired velocity : "<<trajGen.getVelocityTrajectory(realTime)<<std::endl;
+        std::cout<<"desired acceleration : "<<trajGen.getAccelerationTrajectory(realTime)<<std::endl;
+        accumulatedPosition += trajGen.getVelocityTrajectory(realTime) * dT;
+        accumulatedVelocity += trajGen.getAccelerationTrajectory(realTime) * dT;
+        std::cout<<"accumulated position : "<<accumulatedPosition<<std::endl;
+        std::cout<<"accumulated velocity : "<<accumulatedVelocity<<std::endl;
     }
-    std::cout<<accumulatedPosition<<std::endl;
+}
+
+void QuinticTrajectoryTest(double currentPosition, double goalPosition, double currentTime, double timeDuration)
+{
+    QuinticTrajectoryGenerator trajGen;
+    trajGen.updateTrajectory(currentPosition, goalPosition, currentTime, timeDuration);
+    double realTime;
+    double accumulatedPosition = 0.0;
+    double accumulatedVelocity = 0.0;
+    double dT = 0.001;
+    for(int i = 0; i< timeDuration / dT +1; i++)
+    {
+        realTime = i * dT;
+        std::cout<<"iteration : "<<i<<std::endl;
+        std::cout<<"desired position : "<<trajGen.getPositionTrajectory(realTime)<<std::endl;
+        std::cout<<"desired velocity : "<<trajGen.getVelocityTrajectory(realTime)<<std::endl;
+        std::cout<<"desired acceleration : "<<trajGen.getAccelerationTrajectory(realTime)<<std::endl;
+        accumulatedPosition += trajGen.getVelocityTrajectory(realTime) * dT;
+        accumulatedVelocity += trajGen.getAccelerationTrajectory(realTime) * dT;
+        std::cout<<"accumulated position : "<<accumulatedPosition<<std::endl;
+        std::cout<<"accumulated velocity : "<<accumulatedVelocity<<std::endl;
+    }
+}
+int main() {
+//    CubicTrajectoryTest(0.0, 0.53, 0.0, 5.0);
+    QuinticTrajectoryTest(0.0, 0.53, 0.0, 5.0);
+
 }
